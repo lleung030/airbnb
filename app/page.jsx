bff17e3ce0df1556b84e78259b39db8419a06dde
lleung030 <lucasleung2593@gmail.com>
2023-07-24 15:48:13 -0700

    explore nearby padding

--- app/page.jsx
+++ app/page.jsx
@@ -7,6 +7,11 @@ export default function Home() {
     <div>
       <Header />
       <Banner />
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className='pt-6'>
+          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+        </section>
+      </main>
     </div>
   )
 }
