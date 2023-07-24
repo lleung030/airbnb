1c3b84ec7fd536fb105a762af2a1a81ebccf4b1e
lleung030 <lucasleung2593@gmail.com>
2023-07-24 15:24:28 -0700

    airbnb logo/icon

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import Head from 'next/image'
+import Header from '@/components/Header'
+import Banner from '@/components/Banner'
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      
+    </div>
+  )
+}
